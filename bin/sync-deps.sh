3c88de2657a6bd49d852026227bd1b81eedd1e59
damon <damon@mahuika.co.nz>
2018-07-02 14:19:48 -0400

    Remove postinstall

--- bin/sync-deps.sh
+++ bin/sync-deps.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
-YARN_ARGS="--frozen-lockfile --non-interactive --silent"
-
-if [[ ${npm_lifecycle_event} == "postinstall" ]]; then
-	if [[ $(basename $(cd .. && pwd)) != "node_modules" ]]; then
-		# Don't run as a postinstall script unless installed as a dependency
-		exit 0
-	fi
-	# Capture this before we cd below
-	INSTALLED_IN_PKG_DIR=$(cd .. && npm prefix)  # Can't see how else to figure out where is the package.json we're installed into
-fi
+INSTALL_ARGS="--frozen-lockfile --non-interactive --silent"
 
 cd $(git rev-parse --show-toplevel)  # Run everything from the root of the git tree to match what we store in GIT_PATHS
 
@@ -23,21 +14,15 @@ if [[ ${HUSKY_GIT_PARAMS+foo} ]]; then  # https://stackoverflow.com/questions/36
 		# post-merge hook
 		GIT_COMPARE_PATHS=(ORIG_HEAD HEAD)
 	fi
-    SELECTIVE_UPDATE=true
+    # Sync modified files
+    SELECTIVE_UPDATE=1
 	GIT_PATHS=$(git diff-tree -r --name-only --no-commit-id ${GIT_COMPARE_PATHS[@]})
 else
-	# Sync everything
+	# Sync all files
 	GIT_PATHS=$(git ls-tree --full-tree -r --name-only HEAD)
-	if [[ ${npm_lifecycle_event} == "postinstall" ]]; then
-		# Don't try and install the package file that we're already in the middle of installing
-		SKIP_LOCK_PATH=$(git ls-tree --full-name --name-only HEAD "${INSTALLED_IN_PKG_DIR}/yarn.lock")  # So path is same format that we need to match to
-	fi
 fi
 
 echo "${GIT_PATHS}" | grep "\(^\|/\)yarn.lock$" | while read -r LOCK_PATH; do
-	if [[ ${LOCK_PATH} == ${SKIP_LOCK_PATH} ]]; then
-		continue
-	fi
 	PKG_DIR=$(dirname ${LOCK_PATH})
 	if [[ ${SELECTIVE_UPDATE} ]]; then
 		echo Updating dependencies due to modifed ${LOCK_PATH}
@@ -51,8 +36,8 @@ echo "${GIT_PATHS}" | grep "\(^\|/\)yarn.lock$" | while read -r LOCK_PATH; do
 	if [ -e "${PKG_DIR}/.meteor" ]; then
 		# Due to binary compilation differences, meteor projects need to use its exact node version
 		METEOR_NODE=$(cd ${PKG_DIR} && meteor node -e "process.stdout.write(process.execPath)")
-		PATH=$(dirname ${METEOR_NODE}):$PATH yarn install --cwd "${PKG_DIR}" ${YARN_ARGS}
+		PATH=$(dirname ${METEOR_NODE}):$PATH SYNCING_DEPS=1 yarn install --cwd "${PKG_DIR}" ${INSTALL_ARGS}
 	else
-		yarn install --cwd "${PKG_DIR}" ${YARN_ARGS}
+		SYNCING_DEPS=1 yarn install --cwd "${PKG_DIR}" ${INSTALL_ARGS}
 	fi
 done
