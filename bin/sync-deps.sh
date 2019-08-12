a24521cc11f58f280776cec7d2eeb2ebca3e7f4d
damon <damon@mahuika.co.nz>
2019-08-12 14:38:03 +1200

    Add quotes around shell variables

--- bin/sync-deps.sh
+++ bin/sync-deps.sh
@@ -2,17 +2,17 @@
 
 INSTALL_ARGS="--frozen-lockfile --non-interactive --silent --ignore-engines"
 
-cd $(git rev-parse --show-toplevel)  # Run everything from the root of the git tree to match what we store in GIT_PATHS
+cd "$(git rev-parse --show-toplevel)"  # Run everything from the root of the git tree to match what we store in GIT_PATHS
 
 if [[ ${HUSKY_GIT_PARAMS+foo} ]]; then  # https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
     # HUSKY_GIT_PARAMS exists therefore called by husky git hook
 	GIT_PARAMS=(${HUSKY_GIT_PARAMS})  # Turn into array
-    if [[ ${GIT_PARAMS[0]} == "rebase" || ${GIT_PARAMS[0]} == "amend" ]]; then
-        # post-rewrite hook, so check everything
-        :
+  if [[ ${GIT_PARAMS[0]} == "rebase" || ${GIT_PARAMS[0]} == "amend" ]]; then
+    # post-rewrite hook, so check everything
+    :
 	elif [[ ${GIT_PARAMS[1]} ]]; then
 		# post-checkout hook
-        GIT_COMPARE_PATHS=(${GIT_PARAMS[@]:0:2})
+    GIT_COMPARE_PATHS=(${GIT_PARAMS[@]:0:2})
 	else
 		# post-merge hook
 		GIT_COMPARE_PATHS=(ORIG_HEAD HEAD)
@@ -20,8 +20,8 @@ if [[ ${HUSKY_GIT_PARAMS+foo} ]]; then  # https://stackoverflow.com/questions/36
 fi
 
 if [[ ${GIT_COMPARE_PATHS} ]]; then
-    # Sync modified files
-    SELECTIVE_UPDATE=1
+  # Sync modified files
+  SELECTIVE_UPDATE=1
 	GIT_PATHS=$(git diff-tree -r --name-only --no-commit-id ${GIT_COMPARE_PATHS[@]})
 else
 	# Sync all files
@@ -29,20 +29,20 @@ else
 fi
 
 echo "${GIT_PATHS}" | grep "\(^\|/\)yarn.lock$" | while read -r LOCK_PATH; do
-	PKG_DIR=$(dirname ${LOCK_PATH})
+	PKG_DIR=$(dirname "${LOCK_PATH}")
 	if [[ ${SELECTIVE_UPDATE} ]]; then
-		echo Updating dependencies due to modifed ${LOCK_PATH}
+		echo "Updating dependencies due to modifed ${LOCK_PATH}"
 	else
 		if [[ ${PKG_DIR} == "." ]]; then
-			echo Installing root packages
+			echo "Installing root packages"
 		else
-			echo Installing ${PKG_DIR} packages
+			echo "Installing ${PKG_DIR} packages"
 		fi
 	fi
 	if [[ -e "${PKG_DIR}/.meteor" ]]; then
 		# Due to binary compilation differences, meteor projects need to use its exact node version
-		METEOR_NODE=$(cd ${PKG_DIR} && meteor node -e "process.stdout.write(process.execPath)")
-		PATH=$(dirname ${METEOR_NODE}):$PATH SYNCING_DEPS=1 yarn install --cwd "${PKG_DIR}" ${INSTALL_ARGS}
+		METEOR_NODE=$(cd "${PKG_DIR}" && meteor node -e "process.stdout.write(process.execPath)")
+		PATH=$(dirname "${METEOR_NODE}"):$PATH SYNCING_DEPS=1 yarn install --cwd "${PKG_DIR}" ${INSTALL_ARGS}
 	else
 		SYNCING_DEPS=1 yarn install --cwd "${PKG_DIR}" ${INSTALL_ARGS}
 	fi
